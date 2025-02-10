b950880e726cf06f51605a803047cc812e2ac4cd
Arunabh Mishra <38589274+arunabh1904@users.noreply.github.com>
2025-02-09 19:57:21 -0500

    Create 2025-02-09-growing-up.m

---
+++ _posts/2025-02-09-growing-up.m
@@ -0,0 +1,18 @@
+---
+layout: post
+title: "My Journey So Far - Growing Up"
+date: 2025-02-09 00:00:00 -0500
+---
+
+# GROWING UP
+
+I grew up in Jaipur, a city that shaped much of my early life. I lost my father when I was 5, but my mother and sisters never let me feel that I was missing anything growing up. They showed me immense love and support, helping me navigate those challenging early years.
+
+I was a very introverted child, often retreating into my own world. Video games, especially DOTA and DOTA 2, became my escape. I spent countless hours—about **16,000**!—immersed in these games, even ranking **#16 in India** at one point. Over time, I realized that if I channeled the same energy and dedication into other areas of life, I could achieve much more in the ‘real’ world. This insight gradually pulled me out of my shell and brought me to the US.
+
+My education was primarily in Jaipur, but I also spent three years in Kota preparing for the JEE. Kota is often referred to as a **factory** ([Netflix show](https://www.netflix.com/title/81249783) / [Wiki](https://en.wikipedia.org/wiki/Kota_Factory)) because of its intense academic competition. It’s a place where [Girard's mimetic theory of desire](https://en.wikipedia.org/wiki/Mimetic_theory) plays out vividly. Students imitate each other’s drive for success (as well as stress and anxiety), all trying to outdo one another. This environment can be overwhelming—especially for teenagers who aren’t emotionally equipped to handle that level of pressure. I was among the thousands of kids who couldn’t crack the IIT exam. At the time, it felt like the end of the world. The disappointment of letting down my family after years of intense preparation still sends shudders down my spine at 33. You can imagine the toll it took on my 18-year-old self.
+
+### Takeaways
+- **Be resilient** in the face of adversity.  
+- Sometimes, **no matter how hard you try, you will fail**—and that's okay.  
+- **It's okay to indulge** in things you like (e.g., video games), but don’t let them consume you.
